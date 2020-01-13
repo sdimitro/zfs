3895c849cf5d2195e1348341b9600b88d337e342
Serapheim Dimitropoulos <serapheim@delphix.com>
2020-01-13 14:49:04 -0800

    mahrens work - next step

--- lib/libzpool/zfs_user_ioctl.c
+++ lib/libzpool/zfs_user_ioctl.c
@@ -23,10 +23,114 @@
  * Copyright (c) 2013, 2020 by Delphix. All rights reserved.
  */
 
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <sys/zfs_ioctl.h>
+
+
+/*
+ * XXX: Big Theory Statement?
+ */
+
+static pthread_key_t pid_key;
+static pthread_key_t ioctl_key;
+
+typedef struct conn_arg {
+	int conn_fd;
+} conn_arg_t;
+
+static void *
+handle_connection(void *argp)
+{
+	// XXX [NEXT]
+	printf("BOOM!\n");
+	return (NULL);
+}
+
+static void *
+accept_connection(void *argp)
+{
+	int sock_fd = (uintptr_t)argp;
+	int conn_fd;
+	struct sockaddr_un address;
+	socklen_t socklen = sizeof(address);
+
+	/* XXX [mahrens] - kick off a thread for this */
+	while ((conn_fd = accept(sock_fd, (struct sockaddr *)&address,
+	    &socklen)) >= 0) {
+		pthread_t tid;
+
+		conn_arg_t *ca = malloc(sizeof (conn_arg_t));
+		ca->conn_fd = conn_fd;
+		pthread_create(&tid, NULL, handle_connection, ca);
+	}
+
+	if (conn_fd < 0)
+		perror("accept failed");
+
+	return (NULL);
+}
 
 void
 zfs_user_ioctl_init(void)
 {
-	printf("Hello, Userland World!\n");
+	int sock_fd;
+	struct sockaddr_un address = { 0 };
+	char *socket_name;
+	pthread_t tid;
+	int error;
+
+	// XXX [NEXT]:zfs_ioctl_init();
+
+	socket_name = getenv(ZFS_SOCKET_ENVVAR);
+	if (socket_name == NULL)
+		return;
+
+        error = pthread_key_create(&pid_key, NULL);
+        if (error != 0) {
+		perror("pthread_key_create");
+		abort();
+        }
+
+        error = pthread_key_create(&ioctl_key, NULL);
+        if (error != 0) {
+		perror("pthread_key_create");
+		abort();
+        }
+
+	/*
+	 * XXX: Explain why
+	 * XXX: sigignore() may be deprecated.
+	 *      Use whatever the new thing is.
+	 */
+	sigignore(SIGPIPE);
+
+	if ((sock_fd = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(1);
+	}
+
+	address.sun_family = AF_UNIX;
+	// XXX: double-check format issue (see diff)
+	snprintf(address.sun_path, sizeof(address.sun_path), "%s",
+	    socket_name);
+
+	(void) unlink(socket_name);
+	if (bind(sock_fd, (struct sockaddr *)&address,
+	    sizeof(struct sockaddr_un)) != 0) {
+		perror("bind failed");
+		exit(1);
+	}
+
+	if (listen(sock_fd, 2) != 0) {
+		perror("listen failed");
+		exit(1);
+	}
+
+	pthread_create(&tid, NULL, accept_connection,
+	    (void*)(uintptr_t)sock_fd);
 }
