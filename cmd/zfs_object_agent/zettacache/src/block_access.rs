sdimitro/zfs

commit ecf9547a8ebb70cfb12e300ee375575509a86904

read & write threads (#194)
The low-level read and write calls to the disk are done with blocking
pread() and pwrite() system calls, so we can't do them from regular
async Tokio tasks, as that would block the worker thread.  Instead we
dispatch these blocking calls to Tokio's blocking threads.

The problem is that the performance of creating blocking tasks with high
frequency is not great, due to CPU overhead of the tokio scheduler, and
also the overhead of putting threads to sleep and waking them up in
userland (using the futex() syscall).

For example, consider the case where there are always i/o's to process,
and we are at the maximum queue depth (QD) (i.e. there are no permits
left in Disk::outstanding_reads/writes, and tasks are awaiting permits).
There will be QD blocking worker threads each processing one of the
blocking tasks to do an i/o.  When a blocking task completes, there is
no additional task for it to work on (because the limit of the number of
blocking threads is greater than the number of concurrent blocking
tasks), so it has to go to sleep.  The initiating task will get the
response and drop the semaphore, allowing another task to dispatch
another blocking task, waking a blocking worker thread.

We can improve performance a bit by avoiding this frequent sleep-wake of
the worker threads, by using our own dedicated threads for pread() and
pwrite(), sending them messages to indicate what syscalls we need to
make.  When a worker completes pread/pwrite, it may find that there is
more work to do (another message in the queue) so it can start on that
immediately without going to sleep.

We use a Flume MPMC (multiple producer, multiple consumer) channel for
the messages.  The number of threads is the same as the configured
maximum queue depth.

Performance results (on i3en.2x, local NVMe SSD):
  Sequential read of 2 cached files:
  55K -> 75K IOPS (+36%)
  Random reads of uncached file with sibling block ingestion:

cmd/zfs_object_agent/zettacache/src/block_access.rs
@@ -17,6 +17,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::fmt::Debug;
 use std::fmt::Display;
+use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 use std::os::unix::prelude::AsRawFd;
@@ -24,10 +25,8 @@ use std::os::unix::prelude::OpenOptionsExt;
 use std::path::Path;
 use std::sync::atomic::Ordering;
 use std::time::Instant;
-use tokio::fs::File;
-use tokio::sync::Semaphore;
+use tokio::sync::oneshot;
 use util::get_tunable;
-use util::super_trace;
 use util::with_alloctag;
 use util::zettacache_stats::*;
 use util::From64;
@@ -110,13 +109,21 @@ pub struct BlockAccess {
 
 #[derive(Debug)]
 pub struct Disk {
-    file: File,
+    // We want all the reader/writer_threads to share the same file descriptor,
+    // but we don't have a mechanism to ensure that they stop using the fd when
+    // the DiskStruct is dropped and the fd is closed.  To solve this we simply
+    // never close the fd.  The fd is owned by the File, and we leave a
+    // reference to it here to indicate that it's related to this Disk, even
+    // though it's only used via the reader/writer_threads.
+    #[allow(dead_code)]
+    file: &'static File,
+
     device_path: String,
     size: u64,
     sector_size: usize,
     io_stats: DiskIoStats,
-    outstanding_reads: Semaphore,
-    outstanding_writes: Semaphore,
+    reader_tx: flume::Sender<ReadMessage>,
+    writer_tx: flume::Sender<WriteMessage>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -132,47 +139,47 @@ pub enum EncodeType {
     BincodeFixint,
 }
 
-// Generate ioctl function
-nix::ioctl_read!(ioctl_blkgetsize64, 0x12u8, 114u8, u64);
-nix::ioctl_read_bad!(ioctl_blksszget, 0x1268, usize);
-
 #[cfg(target_os = "linux")]
 const CUSTOM_OFLAGS: i32 = libc::O_DIRECT;
 #[cfg(not(target_os = "linux"))]
 const CUSTOM_OFLAGS: i32 = 0;
 
+struct ReadMessage {
+    offset: u64,
+    size: usize,
+    tx: oneshot::Sender<AlignedBytes>,
+}
+
+struct WriteMessage {
+    offset: u64,
+    bytes: AlignedBytes,
+    tx: oneshot::Sender<()>,
+}
+
 impl Disk {
     pub fn new(disk_path: &str, readonly: bool) -> Result<Disk> {
         // Note: using std file open so that this func can be non-async.
         // Although this is blocking from a tokio thread, it's used
-        // infrequently, and we're already blocking from the ioctls below.
-        let std_file = std::fs::OpenOptions::new()
+        // infrequently, and we're already blocking from the ioctls to get the
+        // disk size and block size.
+        let file = std::fs::OpenOptions::new()
             .read(true)
             .write(!readonly)
             .custom_flags(CUSTOM_OFLAGS)
             .open(disk_path)
             .with_context(|| format!("opening disk '{}'", disk_path))?;
-        let file = tokio::fs::File::from_std(std_file);
-        let stat = nix::sys::stat::fstat(file.as_raw_fd()).unwrap();
+        // see comment in `struct Disk`
+        let file: &'static File = Box::leak(Box::new(file));
+        let stat = nix::sys::stat::fstat(file.as_raw_fd())?;
         trace!("stat: {:?}", stat);
         let mode = SFlag::from_bits_truncate(stat.st_mode);
         let sector_size;
         let size;
         if mode.contains(SFlag::S_IFBLK) {
-            size = unsafe {
-                let mut cap: u64 = 0;
-                let cap_ptr = &mut cap as *mut u64;
-                ioctl_blkgetsize64(file.as_raw_fd(), cap_ptr).unwrap();
-                cap
-            };
-            sector_size = unsafe {
-                let mut ssz: usize = 0;
-                let ssz_ptr = &mut ssz as *mut usize;
-                ioctl_blksszget(file.as_raw_fd(), ssz_ptr).unwrap();
-                ssz
-            };
+            size = blkgetsize64(file)?;
+            sector_size = blksszget(file)?;
         } else if mode.contains(SFlag::S_IFREG) {
-            size = u64::try_from(stat.st_size).unwrap();
+            size = u64::try_from(stat.st_size)?;
             sector_size = *MIN_SECTOR_SIZE;
         } else {
             panic!("{}: invalid file type {:?}", disk_path, mode);
@@ -185,24 +192,108 @@ impl Disk {
             .unwrap()
             .to_owned();
 
+        let (reader_tx, reader_rx) = flume::unbounded();
+        let (writer_tx, writer_rx) = flume::unbounded();
+
         let this = Disk {
             file,
             device_path: disk_path.to_string(),
             size,
             sector_size,
             io_stats: DiskIoStats::new(device),
-            outstanding_reads: Semaphore::new(*DISK_READ_MAX_QUEUE_DEPTH),
-            outstanding_writes: Semaphore::new(*DISK_WRITE_MAX_QUEUE_DEPTH),
+            reader_tx,
+            writer_tx,
         };
+
+        for _ in 0..*DISK_READ_MAX_QUEUE_DEPTH {
+            let rx = reader_rx.clone();
+            tokio::task::spawn_blocking(move || {
+                Self::reader_thread(file, sector_size, rx);
+            });
+        }
+        if !readonly {
+            for _ in 0..*DISK_WRITE_MAX_QUEUE_DEPTH {
+                let rx = writer_rx.clone();
+                tokio::task::spawn_blocking(move || {
+                    Self::writer_thread(file, sector_size, rx);
+                });
+            }
+        }
         info!("opening cache file {}: {:?}", disk_path, this);
 
         Ok(this)
     }
+
+    fn reader_thread(file: &'static File, sector_size: usize, rx: flume::Receiver<ReadMessage>) {
+        while let Ok(message) = rx.recv() {
+            let vec = pread_aligned(
+                file,
+                message.offset.try_into().unwrap(),
+                message.size,
+                sector_size,
+            )
+            .unwrap();
+            assert_eq!(vec.len(), message.size);
+            message.tx.send(vec.into()).unwrap();
+        }
+    }
+
+    async fn read(&self, offset: u64, size: usize, io_type: DiskIoType) -> AlignedBytes {
+        self.verify_aligned(offset);
+        self.verify_aligned(size);
+
+        let op = OpInProgress::new(&self.io_stats.stats[io_type]);
+        let (tx, rx) = oneshot::channel();
+        let message = ReadMessage { offset, size, tx };
+
+        self.reader_tx.send_async(message).await.unwrap();
+        let bytes = rx.await.unwrap();
+        op.end(size as u64);
+        bytes
+    }
+
+    fn writer_thread(file: &'static File, sector_size: usize, rx: flume::Receiver<WriteMessage>) {
+        while let Ok(message) = rx.recv() {
+            let offset = i64::try_from(message.offset).unwrap();
+            // Directio requires the pointer to be sector-aligned.  The message
+            // sender aligned it for us if necessary.
+            assert_eq!(message.bytes.alignment() % sector_size, 0);
+            assert_eq!(message.bytes.as_ptr() as usize % sector_size, 0);
+            nix::sys::uio::pwrite(file.as_raw_fd(), &message.bytes, offset).unwrap();
+            message.tx.send(()).unwrap();
+        }
+    }
+
+    async fn write(&self, offset: u64, bytes: AlignedBytes, io_type: DiskIoType) {
+        self.verify_aligned(offset);
+        self.verify_aligned(bytes.len());
+        self.verify_aligned(bytes.alignment());
+
+        let op = OpInProgress::new(&self.io_stats.stats[io_type]);
+        let len = bytes.len();
+        let (tx, rx) = oneshot::channel();
+        let message = WriteMessage { offset, bytes, tx };
+
+        self.writer_tx.send_async(message).await.unwrap();
+        rx.await.unwrap();
+        op.end(len as u64);
+    }
+
+    fn verify_aligned<N: Num + NumCast + Copy + Debug + Display>(&self, n: N) {
+        let sector_size: N = NumCast::from(self.sector_size).unwrap();
+        assert_eq!(
+            n % sector_size,
+            N::zero(),
+            "{} is not sector-aligned ({})",
+            n,
+            sector_size
+        );
+    }
 }
 
-// XXX this is very thread intensive.  On Linux, we can use "glommio" to use
-// io_uring for much lower overheads.  Or SPDK (which can use io_uring or nvme
-// hardware directly).
+// pread/pwrite system calls are not very efficient.  In the future, on Linux,
+// we can use "glommio" to use io_uring for much lower overheads.  Or SPDK
+// (which can use io_uring or nvme hardware directly).
 impl BlockAccess {
     pub fn new(disks: Vec<Disk>, readonly: bool) -> Self {
         let sector_size = disks
@@ -258,48 +349,22 @@ impl BlockAccess {
         self.disks().map(|disk| self.disk_size(disk)).sum()
     }
 
-    // offset and length must be sector-aligned
+    /// The extent.location.offset() and extent.size must be sector-aligned.
+    /// The returned Bytes will also be sector-aligned.
     pub async fn read_raw(&self, extent: Extent, io_type: DiskIoType) -> AlignedBytes {
         self.verify_aligned(extent.location.offset());
         self.verify_aligned(extent.size);
-        let disk = self.disk(extent.location.disk());
-        let fd = disk.file.as_raw_fd();
-        let sector_size = self.sector_size;
-        let begin = Instant::now();
-        let _permit = disk.outstanding_reads.acquire().await.unwrap();
-        let op = OpInProgress::new(&disk.io_stats.stats[io_type]);
-        let bytes: AlignedBytes = tokio::task::spawn_blocking(move || {
-            let mut v = with_alloctag("BlockAccess::raw_read()", || {
-                AlignedVec::with_capacity(usize::from64(extent.size), sector_size)
-            });
-            // By using the unsafe libc::pread() instead of
-            // nix::sys::uio::pread(), we avoid the cost of zeroing out the
-            // vec's buffer.
-            unsafe {
-                let res = libc::pread(
-                    fd,
-                    v.as_mut_ptr() as *mut c_void,
-                    extent.size.try_into().unwrap(),
-                    extent.location.offset().try_into().unwrap(),
-                );
-                let num_bytes_read = usize::try_from(Errno::result(res).unwrap()).unwrap();
-                v.set_len(num_bytes_read);
-            };
-            assert_eq!(v.len() as u64, extent.size);
-            v.into()
-        })
-        .await
-        .unwrap();
-        op.end(bytes.len() as u64);
-        super_trace!(
-            "read({:?}) returned in {}us",
-            extent,
-            begin.elapsed().as_micros()
-        );
-        bytes
+
+        self.disk(extent.location.disk())
+            .read(
+                extent.location.offset(),
+                usize::from64(extent.size),
+                io_type,
+            )
+            .await
     }
 
-    // location.offset and bytes.len() must be sector-aligned.  However,
+    // The location.offset() and bytes.len() must be sector-aligned.  However,
     // bytes.alignment() need not be the sector size (it will be copied if not).
     pub async fn write_raw(
         &self,
@@ -311,35 +376,15 @@ impl BlockAccess {
             !self.readonly,
             "attempting zettacache write in readonly mode"
         );
-        let disk = self.disk(location.disk());
-        let fd = disk.file.as_raw_fd();
-        let length = bytes.len();
-        let offset = location.offset();
-        let alignment = bytes.alignment();
-        self.verify_aligned(offset);
-        self.verify_aligned(length);
-
-        // directio requires the pointer to be sector-aligned
-        if alignment != self.round_up_to_sector(alignment) {
+        self.verify_aligned(location.offset());
+        self.verify_aligned(bytes.len());
+        if bytes.alignment() != self.round_up_to_sector(bytes.alignment()) {
             // XXX copying, this happens for AlignedBytes created from a plain Bytes
-            bytes = AlignedBytes::copy_from_slice(&bytes, self.sector_size)
-        }
-        assert_eq!(bytes.as_ptr() as usize % self.sector_size, 0);
-        let begin = Instant::now();
-        let _permit = disk.outstanding_writes.acquire().await.unwrap();
-        let op = OpInProgress::new(&disk.io_stats.stats[io_type]);
-        tokio::task::spawn_blocking(move || {
-            nix::sys::uio::pwrite(fd, &bytes, i64::try_from(offset).unwrap()).unwrap();
-            super_trace!(
-                "write({:?} len={}) returned in {}us",
-                location,
-                length,
-                begin.elapsed().as_micros()
-            );
-        })
-        .await
-        .unwrap();
-        op.end(length as u64);
+            bytes = AlignedBytes::copy_from_slice(&bytes, self.sector_size);
+        };
+        self.disk(location.disk())
+            .write(location.offset(), bytes, io_type)
+            .await
     }
 
     pub fn round_up_to_sector<N: Num + NumCast + Copy>(&self, n: N) -> N {
@@ -488,4 +533,37 @@ impl BlockAccess {
         })
         .unwrap()
     }
+}
+
+/// Get size of block device.
+fn blkgetsize64(file: &File) -> Result<u64> {
+    nix::ioctl_read!(ioctl_blkgetsize64, 0x12u8, 114u8, u64);
+    let mut cap: u64 = 0;
+    let cap_ptr = &mut cap as *mut u64;
+    unsafe { ioctl_blkgetsize64(file.as_raw_fd(), cap_ptr) }?;
+    Ok(cap)
+}
+
+/// Get sector size of block device.
+fn blksszget(file: &File) -> Result<usize> {
+    nix::ioctl_read_bad!(ioctl_blksszget, 0x1268, usize);
+    let mut ssz: usize = 0;
+    let ssz_ptr = &mut ssz as *mut usize;
+    unsafe { ioctl_blksszget(file.as_raw_fd(), ssz_ptr) }?;
+    Ok(ssz)
+}
+
+/// use pread() to read into an aligned vector
+fn pread_aligned(file: &File, offset: i64, len: usize, alignment: usize) -> Result<AlignedVec> {
+    let mut vec = with_alloctag("pread()", || AlignedVec::with_capacity(len, alignment));
+    let fd = file.as_raw_fd();
+    // By using the unsafe libc::pread() instead of nix::sys::uio::pread(), we
+    // avoid the cost of zeroing out the vec's buffer.  pread() will initialize
+    // up to `len` bytes, which the vec has capacity for.
+    unsafe {
+        let res = libc::pread(fd, vec.as_mut_ptr() as *mut c_void, len, offset);
+        let num_bytes_read = usize::try_from(Errno::result(res)?).unwrap();
+        vec.set_len(num_bytes_read);
+    };
+    Ok(vec)
 }

cmd/zfs_object_agent/util/src/vec_ext.rs
@@ -18,6 +18,7 @@ pub fn tersevec<E>(vec: &[E], fmt: &mut Formatter) -> Result {
     fmt.write_fmt(format_args!("[...{} elements...]", vec.len()))
 }
 
+#[derive(Debug)]
 pub struct AlignedBytes {
     alignment: usize,
     bytes: Bytes,
@@ -88,6 +89,7 @@ impl From<AlignedVec> for AlignedBytes {
     }
 }
 
+#[derive(Debug)]
 pub struct AlignedVec {
     alignment: usize,
     vec: Vec<u8>,

cmd/zfs_object_agent/zettacache/Cargo.toml
@@ -16,6 +16,7 @@ chrono = "0.4"
 conv = "0.3.3"
 derivative = "2.2.0"
 either = "1.6.1"
+flume = "0.10.10"
 futures = "0.3.13"
 futures-core = "0.3.13"
 lazy_static = "1.4.0"

commit 1eeb5ed3ca7a522446a79d351008a0559c7958b5

Fix error with 3+ zettacache devices. (#218)
There was a bug introduced by #194 where if there are 3 or more
zettacache devices, we can't communicate with S3.  The problem is that
we've created many threads with tokio::task::spawn_blocking() (192 per
disk), to handle read/write i/o's to disk, and we've run into the
default limit of 512 tokio blocking threads.  Then when rusoto/reqwest
wants to create a blocking thread, it hangs and eventually hits a
timeout.

The fix is to use std::thread::spawn(), which doesn't have a limit
(beyond what's imposed by the OS, 58344 on a small VM).

cmd/zfs_object_agent/zettacache/src/block_access.rs
@@ -207,14 +207,17 @@ impl Disk {
 
         for _ in 0..*DISK_READ_MAX_QUEUE_DEPTH {
             let rx = reader_rx.clone();
-            tokio::task::spawn_blocking(move || {
+            // note, we want to use a "std" thread here rather than
+            // tokio::task::spawn_blocking() because the latter has a limit of how many
+            // threads it will create (default 512)
+            std::thread::spawn(move || {
                 Self::reader_thread(file, sector_size, rx);
             });
         }
         if !readonly {
             for _ in 0..*DISK_WRITE_MAX_QUEUE_DEPTH {
                 let rx = writer_rx.clone();
-                tokio::task::spawn_blocking(move || {
+                std::thread::spawn(move || {
                     Self::writer_thread(file, sector_size, rx);
                 });
             }