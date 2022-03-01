e8a58edf43b354fc0725eb68533db969a9623ef7
Manoj Joseph <manoj.joseph@delphix.com>
2022-03-01 09:41:41 -0800

    DOSE-1083 Cleanup the error message generated by test_connectivity (#261)

--- cmd/zfs_object_agent/zettaobject/src/test_connectivity.rs
+++ cmd/zfs_object_agent/zettaobject/src/test_connectivity.rs
@@ -1,25 +1,61 @@
-use crate::{ObjectAccess, ObjectAccessOpType};
+use crate::{OAError, ObjectAccess, ObjectAccessOpType};
 use rand::Rng;
+use rusoto_core::RusotoError;
+use serde::Deserialize;
 use std::time::Duration;
 
+#[derive(Debug, Deserialize)]
+struct Error {
+    #[serde(rename = "Code")]
+    code: String,
+    #[serde(rename = "Message")]
+    message: String,
+}
+
 // Test by writing and deleting an object.
-async fn do_test_connectivity(object_access: &ObjectAccess) -> anyhow::Result<()> {
+async fn do_test_connectivity(object_access: &ObjectAccess) -> Result<(), String> {
     let num: u64 = rand::thread_rng().gen();
     let file = format!("test/test_connectivity_{}", num);
     let content = "test connectivity to S3".as_bytes().to_vec();
 
-    object_access
+    match object_access
         .put_object_timed(
             file.clone(),
             content.into(),
             ObjectAccessOpType::MetadataPut,
             Some(Duration::from_secs(30)),
         )
-        .await?;
-
-    object_access.delete_object(file).await;
-
-    Ok(())
+        .await
+    {
+        Err(OAError::RequestError(RusotoError::Unknown(bhr))) => {
+            match serde_xml_rs::from_str::<Error>(bhr.body_as_str()) {
+                Ok(error) => Err(format!(
+                    "Connectivity test failed: {}: {}",
+                    error.code, error.message
+                )),
+                Err(_) => {
+                    // If the error string can not be deserialized as xml, return the enterity of the error back.
+                    Err(format!("Connectivity test failed: {}", bhr.body_as_str()))
+                }
+            }
+        }
+        Err(OAError::TimeoutError(_)) => {
+            Err("Connectivity test failed with a timeout.".to_string())
+        }
+        Err(OAError::RequestError(RusotoError::Service(err))) => Err(format!(
+            "Connectivity test failed due to a service error: {}",
+            err
+        )),
+        Err(OAError::RequestError(RusotoError::Credentials(err))) => Err(format!(
+            "Connectivity test failed due to a credentials error: {}",
+            err
+        )),
+        Err(err) => Err(format!("Connectivity test failed: {}", err)),
+        Ok(_) => {
+            object_access.delete_object(file).await;
+            Ok(())
+        }
+    }
 }
 
 pub fn test_connectivity(
@@ -52,7 +88,7 @@ pub fn test_connectivity(
 
             std::process::exit(match do_test_connectivity(&object_access).await {
                 Err(err) => {
-                    eprintln!("Connectivity test failed: {}", err);
+                    eprintln!("{}", err);
                     1
                 }
                 Ok(_) => {
