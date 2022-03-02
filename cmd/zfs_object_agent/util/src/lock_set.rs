94846570dc9a4b91ea0953de03896a5178047435
Matthew Ahrens <mahrens@delphix.com>
2022-03-02 12:26:10 -0800

    lock_set and watch_once cleanup (#266)
    LockSet can be simplified by using watch_once, and always dropping the
    Sender (without .send()-ing).
    
    Remove some commented-out code in watch_once.rs.

--- cmd/zfs_object_agent/util/src/lock_set.rs
+++ cmd/zfs_object_agent/util/src/lock_set.rs
@@ -1,31 +1,28 @@
+use crate::super_trace;
+use crate::watch_once;
 use dashmap::mapref::entry::Entry;
 use dashmap::DashMap;
 use log::*;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::sync::Arc;
-use tokio::sync::watch;
 
-use crate::super_trace;
-
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Clone)]
 pub struct LockSet<V: Hash + Eq + Copy + Debug> {
-    locks: Arc<DashMap<V, watch::Receiver<()>>>,
+    locks: Arc<DashMap<V, watch_once::Receiver<()>>>,
 }
 
 pub struct LockedItem<V: Hash + Eq + Copy + Debug> {
     value: V,
-    tx: watch::Sender<()>,
+    _tx: watch_once::Sender<()>,
     set: LockSet<V>,
 }
 
 impl<V: Hash + Eq + Copy + Debug> Drop for LockedItem<V> {
     fn drop(&mut self) {
         super_trace!("{:?}: removing lock", self.value);
-        let rx = self.set.locks.remove(&self.value);
-        assert!(rx.is_some());
-        // This unwrap can't fail because there is still a receiver, `rx`.
-        self.tx.send(()).unwrap();
+        self.set.locks.remove(&self.value).unwrap();
+        // self._tx is dropped here, waking any waiting receivers.
     }
 }
 
@@ -44,28 +41,25 @@ impl<V: Hash + Eq + Copy + Debug> LockSet<V> {
 
     pub async fn lock(&self, value: V) -> LockedItem<V> {
         let tx = loop {
-            let mut rx = {
+            let rx = {
                 match self.locks.entry(value) {
                     Entry::Occupied(oe) => oe.get().clone(),
                     Entry::Vacant(ve) => {
-                        let (tx, rx) = watch::channel(());
+                        let (tx, rx) = watch_once::channel();
                         ve.insert(rx);
                         break tx;
                     }
                 }
             };
             super_trace!("{:?}: waiting for existing lock", value);
-            // Note: since we don't hold the locks mutex now, the corresponding
-            // LockedItem may have been dropped, in which case the sender was
-            // dropped.  In this case, the changed() Result will be an Err,
-            // which we ignore with ok().
-            rx.changed().await.ok();
+            // Note: the sender always drops, resulting in an Err, which we ignore with ok().
+            rx.recv().await.ok();
         };
         super_trace!("{:?}: inserted new lock", value);
 
         LockedItem {
             value,
-            tx,
+            _tx: tx,
             set: self.clone(),
         }
     }
--- cmd/zfs_object_agent/util/src/watch_once.rs
+++ cmd/zfs_object_agent/util/src/watch_once.rs
@@ -23,15 +23,6 @@ impl<T: Clone> Sender<T> {
 impl<T: Clone> Receiver<T> {
     /// Receive value, consuming this Receiver.  Returns an error if the Sender was dropped before calling .send().
     pub async fn recv(mut self) -> Result<T, watch::error::RecvError> {
-        /*
-        if let Some(value) = self.0.borrow_and_update().as_ref().cloned() {
-            return Ok(value);
-        }
-        // Note: "else" or "match" statement not allowed here because the
-        // .borrow()'ed Ref would not be dropped until the end of the
-        // else/match.
-        */
-
         match self.0.changed().await {
             // Unwrap is safe because it's only changed to Some.
             Ok(()) => Ok(self.0.borrow().as_ref().unwrap().clone()),
