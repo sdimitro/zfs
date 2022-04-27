sdimitro/zfs

commit 90c7acbb0b64d256c723f1101e8b5f73806f6a9b

code cleanup of BlockBasedLog (#373)
Various minor cleanup of BlockBasedLog code:

Annote entire struct with `#[serde(bound = "T: DeserializeOwned")]` to
indicate that the BlockBasedLogEntry needs to be deserializable without
capturing a reference to the input buffer (i.e. it is not
`Deserialize<'de>`).

Use `#[derivative(Default(bound = "T:"))]` to derive Default rather than
manually implementing it.

Create helper methods to iterate over the Extents that compose the
BlockBasedLog.

ReadOnlySummarizedBlockBasedLog: add a member for the entire Phys,
replacing the individual members copied from the phys.

cmd/zfs_object_agent/zettacache/src/block_based_log/mod.rs
@@ -1,7 +1,6 @@
 pub mod summarized;
 
 use std::cmp::max;
-use std::cmp::min;
 use std::fmt::Debug;
 use std::iter;
 use std::marker::PhantomData;
@@ -9,7 +8,6 @@ use std::ops::Add;
 use std::ops::Sub;
 use std::sync::Arc;
 
-use anyhow::Context;
 use bytesize::ByteSize;
 use derivative::Derivative;
 use futures::stream;
@@ -25,6 +23,7 @@ use util::measure;
 use util::tunable;
 use util::with_alloctag;
 use util::zettacache_stats::DiskIoType;
+use util::From64;
 
 use crate::base_types::*;
 use crate::block_access::BlockAccess;
@@ -59,12 +58,13 @@ tunable! {
 }
 
 pub trait BlockBasedLogEntry:
-    'static + Serialize + DeserializeOwned + Copy + Clone + Unpin + Send + Sync
+    'static + Serialize + DeserializeOwned + Debug + Copy + Clone + Unpin + Send + Sync
 {
 }
 
 #[derive(Derivative, Serialize, Deserialize, Debug, Clone)]
 #[derivative(Default(bound = "T:"))]
+#[serde(bound = "T: DeserializeOwned")]
 pub struct BlockBasedLogPhys<T: BlockBasedLogEntry> {
     slabs: Vec<SlabId>,
     next_chunk: ChunkId,
@@ -74,10 +74,10 @@ pub struct BlockBasedLogPhys<T: BlockBasedLogEntry> {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(bound = "T: DeserializeOwned")]
 pub struct BlockBasedLogChunk<T: BlockBasedLogEntry> {
     id: ChunkId,
     offset: LogOffset,
-    #[serde(bound(deserialize = "Vec<T>: DeserializeOwned"))]
     entries: Vec<T>,
 }
 
@@ -115,6 +115,42 @@ impl<T: BlockBasedLogEntry> BlockBasedLogPhys<T> {
         }
     }
 
+    fn written_extents<'a>(
+        &'a self,
+        slab_access: &'a SlabAccess,
+    ) -> impl DoubleEndedIterator<Item = (LogOffset, Extent)> + 'a {
+        self.allocated_extents(slab_access)
+            .map(|(offset, extent)| (offset, extent.trim_end(self.next_chunk_offset - offset)))
+    }
+
+    fn allocated_extents<'a>(
+        &'a self,
+        slab_access: &'a SlabAccess,
+    ) -> impl DoubleEndedIterator<Item = (LogOffset, Extent)> + 'a {
+        self.slabs.iter().enumerate().map(|(slab_index, &slab_id)| {
+            let offset = LogOffset((slab_index as u64) * slab_access.slab_size());
+            let extent = slab_access.slab_id_to_extent(slab_id);
+            (offset, extent)
+        })
+    }
+
+    fn next_extent_to_write(&self, slab_access: &SlabAccess) -> Option<Extent> {
+        self.allocated_extents(slab_access)
+            .last()
+            .map(|(offset, extent)| extent.trim_start(self.next_chunk_offset - offset))
+            .filter(|extent| extent.size > 0)
+    }
+
+    fn offset_to_location(&self, slab_access: &SlabAccess, offset: LogOffset) -> DiskLocation {
+        let slab_size = slab_access.slab_size();
+        let slab_index = usize::from64(offset.0 / slab_size);
+        let relative_offset = offset.0 % slab_size;
+        slab_access
+            .slab_id_to_extent(self.slabs[slab_index])
+            .location
+            + relative_offset
+    }
+
     // Since &self is not captured by the returned Stream (its extent list is cloned), callers
     // must ensure that the disk space represented by the extents is not overwritten before the
     // stream terminates.  i.e. do not call .clear().
@@ -123,17 +159,9 @@ impl<T: BlockBasedLogEntry> BlockBasedLogPhys<T> {
         block_access: Arc<BlockAccess>,
         slab_access: &SlabAccess,
     ) -> impl Stream<Item = BlockBasedLogChunk<T>> {
-        let slab_size = slab_access.slab_size();
         let extents = self
-            .slabs
-            .iter()
-            .enumerate()
-            .map(|(slab_index, &slab_id)| {
-                // truncate last extent to log size
-                let offset = LogOffset(slab_index as u64 * slab_size);
-                let extent = slab_access.slab_id_to_extent(slab_id);
-                extent.range(0, min(extent.size, self.next_chunk_offset - offset))
-            })
+            .written_extents(slab_access)
+            .map(|(_, extent)| extent)
             .collect::<Vec<_>>();
 
         // Just buffer a single (16MB) extent between the two tasks.
@@ -160,20 +188,17 @@ impl<T: BlockBasedLogEntry> BlockBasedLogPhys<T> {
 
         let next_chunk = self.next_chunk;
         measure!("BlockBasedLogPhys::iter_chunks() deserializer").spawn(async move {
-            let mut chunk_id = ChunkId(0);
             while let Some(extent_bytes) = extent_rx.recv().await {
                 let mut total_consumed = 0;
                 while total_consumed < extent_bytes.len() {
                     // XXX handle checksum error here
                     let (chunk, consumed): (BlockBasedLogChunk<T>, usize) = block_access
                         .chunk_from_raw(&extent_bytes[total_consumed..])
-                        .with_context(|| format!("{:?}", chunk_id))
                         .unwrap();
-                    assert_eq!(chunk.id, chunk_id);
+                    let chunk_id = chunk.id;
                     if chunk_tx.send(chunk).await.is_err() {
                         break;
                     }
-                    chunk_id = chunk_id.next();
                     total_consumed += consumed;
                     if chunk_id == next_chunk {
                         break;
@@ -204,19 +229,6 @@ impl<T: BlockBasedLogEntry> BlockBasedLogPhys<T> {
 
     pub fn capacity_bytes(&self, slab_access: &SlabAccess) -> u64 {
         self.slabs.len() as u64 * slab_access.slab_size()
-    }
-
-    fn next_write_location(&self, slab_allocator: &SlabAllocator) -> Option<Extent> {
-        self.slabs
-            .last()
-            .map(|&slab_id| {
-                let slab_offset =
-                    LogOffset((self.slabs.len() - 1) as u64 * slab_allocator.slab_size());
-                let offset_within_extent = self.next_chunk_offset - slab_offset;
-                let extent = slab_allocator.slab_id_to_extent(slab_id);
-                extent.range(offset_within_extent, extent.size - offset_within_extent)
-            })
-            .filter(|extent| extent.size > 0)
     }
 }
 
@@ -299,7 +311,7 @@ impl<T: BlockBasedLogEntry> BlockBasedLog<T> {
 
             // XXX I think we only want to use Bincode for the main index?
             let raw_chunk = self.block_access.chunk_to_raw(EncodeType::Bincode, &chunk);
-            let extent = match self.phys.next_write_location(&self.slab_allocator) {
+            let extent = match self.phys.next_extent_to_write(self.slab_allocator.access()) {
                 Some(extent) if extent.size >= raw_chunk.len() as u64 => extent,
                 Some(_) => {
                     // Not enough space at end of the current slab, try a smaller chunk.  We need
@@ -361,15 +373,19 @@ impl<T: BlockBasedLogEntry> BlockBasedLog<T> {
 pub struct LogOffset(u64);
 
 impl Add<usize> for LogOffset {
-    type Output = LogOffset;
-
+    type Output = Self;
     fn add(self, rhs: usize) -> Self::Output {
-        LogOffset(self.0 + rhs as u64)
+        self + rhs as u64
+    }
+}
+impl Add<u64> for LogOffset {
+    type Output = Self;
+    fn add(self, rhs: u64) -> Self::Output {
+        Self(self.0 + rhs)
     }
 }
 impl Sub<LogOffset> for LogOffset {
     type Output = u64;
-
     fn sub(self, rhs: LogOffset) -> Self::Output {
         self.0 - rhs.0
     }

cmd/zfs_object_agent/zettacache/src/base_types.rs
@@ -1,4 +1,5 @@
 use std::borrow::Borrow;
+use std::cmp::min;
 use std::fmt::*;
 use std::num::NonZeroU64;
 use std::ops::Add;
@@ -149,6 +150,24 @@ impl Extent {
         Extent {
             location: self.location + relative_offset,
             size,
+        }
+    }
+
+    /// Trims the beginning off of this extent.
+    pub fn trim_start(&self, relative_offset: u64) -> Extent {
+        assert_le!(relative_offset, self.size);
+        Extent {
+            location: self.location + relative_offset,
+            size: self.size - relative_offset,
+        }
+    }
+
+    /// Reduce size to specified value.  If size is larger than the extent's size, this is a
+    /// no-op.
+    pub fn trim_end(&self, size: u64) -> Extent {
+        Extent {
+            location: self.location,
+            size: min(self.size, size),
         }
     }
 

cmd/zfs_object_agent/zettacache/src/block_based_log/summarized.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Instant;
 
+use derivative::Derivative;
 use futures::future::join;
 use futures::StreamExt;
 use futures_core::Stream;
@@ -40,28 +41,27 @@ tunable! {
     static ref CHUNK_CACHE_ENTRIES: usize = 128;
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Derivative, Debug, Clone)]
+#[derivative(Default(bound = "T:"))]
+#[serde(bound = "T: DeserializeOwned")]
 pub struct SummarizedBlockBasedLogPhys<T: BlockBasedLogEntry> {
-    #[serde(bound(deserialize = "T: DeserializeOwned"))]
     this: BlockBasedLogPhys<T>,
-    #[serde(bound(deserialize = "T: DeserializeOwned"))]
     chunk_summary: BlockBasedLogPhys<BlockBasedLogChunkSummaryEntry<T>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+#[serde(bound = "T: DeserializeOwned")]
 pub struct BlockBasedLogChunkSummaryEntry<T: BlockBasedLogEntry> {
     offset: LogOffset,
-    #[serde(bound(deserialize = "T: DeserializeOwned"))]
     first_entry: T,
 }
 impl<T: BlockBasedLogEntry> OnDisk for BlockBasedLogChunkSummaryEntry<T> {}
 impl<T: BlockBasedLogEntry> BlockBasedLogEntry for BlockBasedLogChunkSummaryEntry<T> {}
 
 pub struct ReadOnlySummarizedBlockBasedLog<T: BlockBasedLogEntry> {
+    phys: SummarizedBlockBasedLogPhys<T>,
     block_access: Arc<BlockAccess>,
     slab_allocator: Arc<SlabAllocator>,
-    this: BlockBasedLogPhys<T>,
-    chunk_summary: BlockBasedLogPhys<BlockBasedLogChunkSummaryEntry<T>>,
     chunks: Vec<BlockBasedLogChunkSummaryEntry<T>>,
     chunk_cache: Mutex<LruCache<ChunkId, BlockBasedLogChunk<T>>>,
     chunk_reads: LockSet<ChunkId>,
@@ -71,15 +71,6 @@ pub struct SummarizedBlockBasedLog<T: BlockBasedLogEntry> {
     readonly: ReadOnlySummarizedBlockBasedLog<T>,
     this: BlockBasedLog<T>,
     chunk_summary: BlockBasedLog<BlockBasedLogChunkSummaryEntry<T>>,
-}
-
-impl<T: BlockBasedLogEntry> Default for SummarizedBlockBasedLogPhys<T> {
-    fn default() -> Self {
-        Self {
-            this: Default::default(),
-            chunk_summary: Default::default(),
-        }
-    }
 }
 
 pub struct BlockBasedLogValueGuard<'a, T: BlockBasedLogEntry> {
@@ -163,10 +154,9 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
         );
 
         Self {
+            phys,
             block_access,
             slab_allocator,
-            this: phys.this,
-            chunk_summary: phys.chunk_summary,
             chunks,
             chunk_cache: Mutex::new(LruCache::new(*CHUNK_CACHE_ENTRIES)),
             chunk_reads: Default::default(),
@@ -174,14 +164,11 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
     }
 
     pub fn get_phys(&self) -> SummarizedBlockBasedLogPhys<T> {
-        SummarizedBlockBasedLogPhys {
-            this: self.this.clone(),
-            chunk_summary: self.chunk_summary.clone(),
-        }
+        self.phys.clone()
     }
 
     pub fn len(&self) -> u64 {
-        self.this.num_entries
+        self.phys.this.num_entries
     }
 
     #[allow(dead_code)]
@@ -191,17 +178,19 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
 
     /// Size of the on-disk representation
     pub fn num_bytes(&self) -> u64 {
-        self.this.bytes() + self.chunk_summary.bytes()
+        self.phys.this.bytes() + self.phys.chunk_summary.bytes()
     }
 
     /// Iterates the on-disk state; panics if there are pending changes.
     pub fn iter(&self) -> impl Stream<Item = T> {
-        self.this
+        self.phys
+            .this
             .iter(self.block_access.clone(), self.slab_allocator.access())
     }
 
     pub fn iter_chunks(&self) -> impl Stream<Item = BlockBasedLogChunk<T>> {
-        self.this
+        self.phys
+            .this
             .iter_chunks(self.block_access.clone(), self.slab_allocator.access())
     }
 
@@ -210,16 +199,38 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
         let chunk_id = usize::from64(chunk_id.0);
         let chunk_summary = self.chunks[chunk_id];
         let chunk_size = if chunk_id == self.chunks.len() - 1 {
-            self.this.next_chunk_offset - chunk_summary.offset
+            self.phys.this.next_chunk_offset - chunk_summary.offset
         } else {
             self.chunks[chunk_id + 1].offset - chunk_summary.offset
         };
 
-        let slab_size = self.slab_allocator.slab_size();
-        let extent = self
-            .slab_allocator
-            .slab_id_to_extent(self.this.slabs[usize::from64(chunk_summary.offset.0 / slab_size)]);
-        extent.range(chunk_summary.offset.0 % slab_size, chunk_size)
+        Extent {
+            location: self
+                .phys
+                .this
+                .offset_to_location(self.slab_allocator.access(), chunk_summary.offset),
+            size: chunk_size,
+        }
+    }
+
+    /// Return the ChunkId where this key will be found, if present.
+    fn lookup_chunk_by_key<B, F>(&self, key: &B, f: &mut F) -> Option<ChunkId>
+    where
+        B: Ord + Debug,
+        F: FnMut(&T) -> B,
+    {
+        assert_eq!(ChunkId(self.chunks.len() as u64), self.phys.this.next_chunk);
+
+        // Find the chunk_id that this key belongs in.
+        match self
+            .chunks
+            .binary_search_by_key(key, |chunk_summary| f(&chunk_summary.first_entry))
+        {
+            Ok(index) => Some(ChunkId(index as u64)),
+            // key is before the first chunk, therefore not present
+            Err(index) if index == 0 => None,
+            Err(index) => Some(ChunkId(index as u64 - 1)),
+        }
     }
 
     /// Returns (value, chunk_cache_hit), where the value is the value corresponding
@@ -230,17 +241,9 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
         B: Ord + Debug,
         F: FnMut(&T) -> B,
     {
-        assert_eq!(ChunkId(self.chunks.len() as u64), self.this.next_chunk);
-
-        // Find the chunk_id that this key belongs in.
-        let chunk_id = match self
-            .chunks
-            .binary_search_by_key(key, |chunk_summary| f(&chunk_summary.first_entry))
-        {
-            Ok(index) => ChunkId(index as u64),
-            // key is before the first chunk, therefore not present
-            Err(index) if index == 0 => return (None, false),
-            Err(index) => ChunkId(index as u64 - 1),
+        let chunk_id = match self.lookup_chunk_by_key(key, &mut f) {
+            Some(chunk_id) => chunk_id,
+            None => return (None, false),
         };
 
         if let Some(chunk) = self.chunk_cache.lock().unwrap().get(&chunk_id) {
@@ -340,13 +343,12 @@ impl<T: BlockBasedLogEntry> ReadOnlySummarizedBlockBasedLog<T> {
         phys: SummarizedBlockBasedLogPhys<T>,
         delta: &SummarizedBlockBasedLogFlushDelta<T>,
     ) {
-        assert_eq!(delta.first_new_chunk, self.this.next_chunk);
+        assert_eq!(delta.first_new_chunk, self.phys.this.next_chunk);
         with_alloctag("ReadOnlySummarizedBlockBasedLog.chunks", || {
             self.chunks.extend_from_slice(&delta.new_chunks)
         });
 
-        self.this = phys.this;
-        self.chunk_summary = phys.chunk_summary;
+        self.phys = phys;
     }
 }
 

cmd/zfs_object_agent/zettacache/src/index.rs
@@ -39,7 +39,7 @@ impl IndexKey {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, Ord, PartialOrd)]
+#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq)]
 #[repr(packed)]
 pub struct IndexValue {
     location: Option<DiskLocation>,
@@ -338,7 +338,7 @@ impl IndexRun {
         self.log.clear();
     }
 
-    // Logically remove entries at and before `first`, which must be >= the current
+    // Logically remove entries at and before `trim_key`, which must be >= the current
     // `trim_key`.  The newly-obsoleted entries must have the provided
     // histogram.
     pub fn trim(&mut self, trim_key: IndexKey, obsoleted: &AtimeHistogramPhys) {

cmd/zfs_object_agent/zettaobject/src/object_based_log.rs
@@ -74,12 +74,12 @@ impl<T: ObjectBasedLogEntry> ObjectBasedLogPhys<T> {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(bound = "T: DeserializeOwned")]
 struct ObjectBasedLogChunk<T: ObjectBasedLogEntry> {
     guid: PoolGuid,
     generation: u64,
     chunk: u64,
     txg: Txg,
-    #[serde(bound(deserialize = "Vec<T>: DeserializeOwned"))]
     entries: Vec<T>,
 }
 impl<T: ObjectBasedLogEntry> OnDisk for ObjectBasedLogChunk<T> {}

commit 938b418c269fe428c9a8c39f91575931af20f47e

fix performance of BlockBasedLogPhys::next_extent_to_write() (#383)
`BlockBasedLogPhys::next_extent_to_write()` uses `Iterator::last()` to
find the last extent.  Unfortunately, this is O(n), iterating over all
the elements to find the last one, even for `DoubleEndedIterator`s.
This can have a large impact on the performance of merging a large index
(where `n` is nontrivial).

This commit changes it to use `DoubleEndedIterator::next_back()`, which
is O(1).

The problem was introduced by #373.

cmd/zfs_object_agent/zettacache/src/block_based_log/mod.rs
@@ -136,7 +136,7 @@ impl<T: BlockBasedLogEntry> BlockBasedLogPhys<T> {
 
     fn next_extent_to_write(&self, slab_access: &SlabAccess) -> Option<Extent> {
         self.allocated_extents(slab_access)
-            .last()
+            .next_back()
             .map(|(offset, extent)| extent.trim_start(self.next_chunk_offset - offset))
             .filter(|extent| extent.size > 0)
     }