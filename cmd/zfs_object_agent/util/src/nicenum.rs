7f5c4dc39aa1c32aa439694d272d0fbec1f76066
Matthew Ahrens <mahrens@delphix.com>
2022-05-06 12:12:57 -0700

    Revert "nice_p2size impl Display" (#411)
    When implementing the Display trait, support for "formatting parameters"
    (e.g. width, alignment, fill, precision) must be implemented manually,
    e.g. by checking `Formatter::width()`.  Alternatively, the object can be
    formatted to a String, and then the formatting parameters can be used on
    the string.
    
    Unfortunately, we rely on these "formatting parameters" (e.g. width,
    alignment) for the `zcache stats` output, but they were not implemented
    when changing nice_p2size (and friends) to return `impl Display`.
    
    This reverts commit c3d6d4dc963b48fca2a6fcba1dcbd105ccfc0214, so that
    these functions again return String which does implement the width and
    alignment formatting parameters.

--- cmd/zfs_object_agent/util/src/nicenum.rs
+++ cmd/zfs_object_agent/util/src/nicenum.rs
@@ -1,107 +1,86 @@
 //! Functions which provide "nice" looking (human-readable) output for numbers.
 
-use std::fmt::Display;
-use std::fmt::Formatter;
-use std::fmt::Result;
 use std::time::Duration;
 
-/// Convert an arbitrary integer into a scaled byte length specification with up to 4 significant
-/// digits (returned string will be <= 6 characters).
-pub fn nice_p2size(number: u64) -> impl Display {
-    struct DelayedFormat(u64);
-    impl Display for DelayedFormat {
-        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-            let mut scaled: f64 = self.0 as f64;
-            for unit in ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"] {
-                // Note: !format() will "round" floating point data to the precision requested,
-                // so the comparisons below take that into account when computing places
-                let places = match scaled {
-                    x if x <= 9.995 => 2,
-                    x if x <= 99.95 => 1,
-                    x if x <= 1023.5 => 0,
-                    _ => {
-                        scaled /= 1024.0;
-                        continue;
-                    }
-                };
-                return write!(f, "{:.*}{}", places, scaled, unit);
+/// Convert an arbitrary integer into a scaled byte length specification
+/// with up to 4 significant digits (returned string will be <= 6 characters).
+pub fn nice_p2size(number: u64) -> String {
+    let mut scaled: f64 = number as f64;
+    for unit in ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"] {
+        // Note: !format() will "round" floating point data to the precision requested,
+        // so the comparisons below take that into account when computing places
+        let places = match scaled {
+            x if x <= 9.995 => 2,
+            x if x <= 99.95 => 1,
+            x if x <= 1023.5 => 0,
+            _ => {
+                scaled /= 1024.0;
+                continue;
             }
-            // it isn't possible to get here. 64 bits can only "count" to exabytes
-            panic!(
-                "Coding error encountered; original number: {}, scaled value: {}",
-                self.0, scaled
-            );
-        }
+        };
+        return format!("{:.*}{}", places, scaled, unit);
     }
-    DelayedFormat(number)
+    // it isn't possible to get here. 64 bits can only "count" to exabytes
+    panic!(
+        "Coding error encountered; original number: {}, scaled value: {}",
+        number, scaled
+    );
 }
 
-/// Format a counter value with up to 4 significant digits and the appropriate unit scale.  The
-/// returned string will fit in 5 characters.
-pub fn nice_number_count(number: f64) -> impl Display {
-    struct DelayedFormat(f64);
-    impl Display for DelayedFormat {
-        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-            let mut scaled = self.0;
-            for unit in ["", "K", "M", "G", "T", "P", "E", "Z", "Y"] {
-                // Note: format!() will "round" floating point data to the precision requested,
-                // so the comparisons below take that into account when computing places
-                let places = match scaled {
-                    x if x <= 9.995 => 2,
-                    x if x <= 99.95 => 1,
-                    x if x <= 999.5 => 0,
-                    _ => {
-                        scaled /= 1000.0;
-                        continue;
-                    }
-                };
-                return write!(f, "{:.*}{}", places, scaled, unit);
+/// Format a counter value with up to 4 significant digits and the appropriate
+/// unit scale.  The returned string will fit in 5 characters.
+pub fn nice_number_count(number: f64) -> String {
+    let mut scaled = number;
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z", "Y"] {
+        // Note: format!() will "round" floating point data to the precision requested,
+        // so the comparisons below take that into account when computing places
+        let places = match scaled {
+            x if x <= 9.995 => 2,
+            x if x <= 99.95 => 1,
+            x if x <= 999.5 => 0,
+            _ => {
+                scaled /= 1000.0;
+                continue;
             }
-            // it isn't possible to get here.
-            panic!("Coding error encountered; original number: {}", self.0);
-        }
+        };
+        return format!("{:.*}{}", places, scaled, unit);
     }
-    DelayedFormat(number)
+    // it isn't possible to get here.
+    panic!("Coding error encountered; original number: {}", number);
 }
 
-/// Format a time **duration** with up to 4 significant digits and the appropriate time unit
-/// between 'ns' up to 's' (seconds).  The returned string will fit in 5 characters unless the
-/// represented time is > 9,999 seconds.
-pub fn nice_number_time(time: Duration) -> impl Display {
-    struct DelayedFormat(Duration);
-    impl Display for DelayedFormat {
-        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-            let nanoseconds: u64 = self.0.as_nanos().try_into().unwrap();
-            if nanoseconds == 0 {
-                // Don't print zero latencies since they're invalid
-                return write!(f, "-");
-            }
-            let mut scaled: f64 = nanoseconds as f64;
-            for unit in ["ns", "us", "ms", "s"] {
-                let places = match scaled {
-                    // Note: format!() will "round" floating point data to the precision requested,
-                    // so the comparisons below take that into account when computing places
-                    x if x <= 9.995 => 2,
-                    x if x <= 99.95 => 1,
-                    x if x <= 999.5 => 0,
-                    _ => {
-                        if unit != "s" {
-                            scaled /= 1000.0;
-                            continue;
-                        } else {
-                            0
-                        }
-                    }
-                };
-                return write!(f, "{:.*}{}", places, scaled, unit);
-            }
-            panic!(
-                "Coding error encountered; original number: {}, scaled value: {}",
-                nanoseconds, scaled
-            );
-        }
+/// Format a time **duration** with up to 4 significant digits and the appropriate
+/// time unit between 'ns' up to 's' (seconds).  The returned string will fit in
+/// 5 characters unless the represented time is > 9,999 seconds.
+pub fn nice_number_time(time: Duration) -> String {
+    let nanoseconds: u64 = time.as_nanos().try_into().unwrap();
+    if nanoseconds == 0 {
+        // Don't print zero latencies since they're invalid
+        return String::from("-");
     }
-    DelayedFormat(time)
+    let mut scaled: f64 = nanoseconds as f64;
+    for unit in ["ns", "us", "ms", "s"] {
+        let places = match scaled {
+            // Note: format!() will "round" floating point data to the precision requested,
+            // so the comparisons below take that into account when computing places
+            x if x <= 9.995 => 2,
+            x if x <= 99.95 => 1,
+            x if x <= 999.5 => 0,
+            _ => {
+                if unit != "s" {
+                    scaled /= 1000.0;
+                    continue;
+                } else {
+                    0
+                }
+            }
+        };
+        return format!("{:.*}{}", places, scaled, unit);
+    }
+    panic!(
+        "Coding error encountered; original number: {}, scaled value: {}",
+        nanoseconds, scaled
+    );
 }
 
 /// Verify that rounding is not breaking our character limit contract.
@@ -118,11 +97,11 @@ fn test_char_limit_for_nice_number() {
         (1024 * 1024 * 1024 * 1024 * 10) - 1,
         (1024 * 1024 * 1024 * 1024 * 100) - 1,
     ] {
-        let nice = nice_p2size(x).to_string();
+        let nice = nice_p2size(x);
         if nice.len() > 6 {
             panic!("{} exceeds 6 chars", nice);
         }
-        let nice = nice_number_count(x as f64).to_string();
+        let nice = nice_number_count(x as f64);
         if nice.len() > 5 {
             panic!("{} exceeds 5 chars", nice);
         }
@@ -134,7 +113,7 @@ fn test_char_limit_for_nice_number() {
 #[test]
 fn test_char_limit_for_nice_time() {
     for n in [9999, 99999, 9999999, 99999999] {
-        let nice = nice_number_time(Duration::from_nanos(n)).to_string();
+        let nice = nice_number_time(Duration::from_nanos(n));
         if nice.len() > 6 {
             panic!("{} exceeds 6 chars", nice);
         }
--- cmd/zfs_object_agent/util/src/zettacache_stats.rs
+++ cmd/zfs_object_agent/util/src/zettacache_stats.rs
@@ -74,7 +74,7 @@ impl StatCount {
         let nice_value = if value == 0.0 || (scale.is_none() && value < 999.0) {
             self.0.load(Relaxed).to_string()
         } else {
-            nice_number_count(value).to_string()
+            nice_number_count(value)
         };
 
         // right aligned for a width of 6, padded with 2 spaces
@@ -117,13 +117,14 @@ impl StatBytes {
         if let Some(s) = scale {
             value *= s;
         }
-        if value < 1.0 {
+        let nice_value = if value < 1.0 {
             // Intentionally avoid displaying "0B" when 0
-            write_stdout!("{:>6}  ", "0");
+            String::from("0")
         } else {
-            // right aligned for a width of 6, padded with 2 spaces
-            write_stdout!("{:>6}  ", nice_p2size(value.round().to_u64().unwrap()));
-        }
+            nice_p2size(value.round().to_u64().unwrap())
+        };
+        // right aligned for a width of 6, padded with 2 spaces
+        write_stdout!("{:>6}  ", nice_value);
     }
 }
 
